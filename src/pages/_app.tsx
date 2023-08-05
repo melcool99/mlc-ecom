705a9f400ab8927da8a8752d78d4e8abc317a276
Cristian <vladcristian84@gmail.com>
2023-08-05 17:35:16 +0300

    added NavBar and Model for DB

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import NavBar from "~/components/NavBar";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <NavBar />
       <Component {...pageProps} />
     </ClerkProvider>
   );
